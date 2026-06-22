b140133392f0d2a8b0350b521a88b22b61201899
Rupesh Thapa <rupeshthapa@Rupeshs-MacBook-Air.local>
2026-06-22 17:21:59 +0545

    Checking ui and adding data on table

--- tests/table.spec.js
+++ tests/table.spec.js
@@ -5,12 +5,14 @@ test.beforeEach(async({page}) => {
 });
 
 test('UI', async({ page }) => {
+    //Modal Open
     await page.getByRole('button', {name: 'Add'}).click();
 
     
-
+    //Getting the class name of modal
     const modal = page.locator('.modal-content');
 
+    //Checking texts and close btn are visible 
     await expect(modal.getByText('Registration Form')).toBeVisible();
     await expect(modal.locator('.btn-close')).toBeVisible();
     await expect(modal.getByText('First Name')).toBeVisible();
@@ -20,6 +22,7 @@ test('UI', async({ page }) => {
     await expect(modal.getByText('Salary')).toBeVisible();
     await expect(modal.getByText('Department')).toBeVisible();
 
+    //Checking input fields and submit are visible or not
     await expect(modal.getByPlaceholder('First Name')).toBeVisible();
     await expect(modal.getByPlaceholder('Last Name')).toBeVisible();
     await expect(modal.getByPlaceholder('name@example.com')).toBeVisible();
@@ -27,10 +30,11 @@ test('UI', async({ page }) => {
     await expect(modal.getByPlaceholder('Salary')).toBeVisible();
     await expect(modal.getByPlaceholder('Department')).toBeVisible();
 
+
     await expect(modal.getByRole('button', {name: 'Submit'})).toBeVisible();
     await page.waitForTimeout(3000);
 });
-
+//   For validation Check
 //   test('Validation', async({ page }) => {
 //   await page.getByRole('button', { name: 'Log in' }).click();
 //   await page.getByRole('button', { name: 'Submit' }).click();
@@ -38,3 +42,30 @@ test('UI', async({ page }) => {
 //   await expect(errorMessage).toBeVisible();
 //   await expect(errorMessage).toHaveText('This field is required');
 // });
+
+test('Submit', async({page}) => {
+    await page.getByRole('button', {name: 'Add'}).click();
+    await page.getByPlaceholder('First Name').fill('Rupesh');
+    await page.getByPlaceholder('Last Name').fill('Thapa');
+    await page.getByPlaceholder('name@example.com').fill('rupesh@gmail.com');
+    await page.getByPlaceholder('Age').fill('25');
+    await page.getByPlaceholder('Salary').fill('5000');
+    await page.getByPlaceholder('Department').fill('QA');
+    await page.getByRole('button', {name: 'Submit'}).click();
+
+    await expect(page.locator('.modal-content')).not.toBeVisible();
+
+    await expect(page.getByRole('cell', {name: 'Rupesh', exact: true})).toBeVisible();
+    await expect(page.getByRole('cell', {name: 'Thapa', exact: true})).toBeVisible();
+    await expect(page.getByRole('cell', {name: 'rupesh@gmail.com', exact: true})).toBeVisible();
+    await expect(page.getByRole('cell', {name: '25', exact: true})).toBeVisible();
+    await expect(page.getByRole('cell', {name: '5000', exact: true})).toBeVisible();
+    await expect(page.getByRole('cell', {name: 'QA', exact: true})).toBeVisible();
+
+
+    await page.waitForTimeout(3000);
+
+
+});
+
+
